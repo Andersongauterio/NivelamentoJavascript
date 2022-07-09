ae4796a1e4afbcb6db58e5eef18eaa391509311c
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-07-09 18:52:40 -0300

    modulos

---
+++ modulos/js/script.js
@@ -0,0 +1,10 @@
+import {sum, round } from './number.js';
+import Product from './product.js';
+
+const x = sum(3, 4);
+
+console.log(x);
+
+const obj = new Product("Computador", 800.0, 10);
+
+console.log(obj);
